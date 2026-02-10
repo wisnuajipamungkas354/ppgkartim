16f22e56eeacb416c19974fd4b6fc5b873c4d046
Wisnu Aji Pamungkas <wisnuajipamungkas354@gmail.com>
2026-02-10 21:00:28 +0700

    Fix label create and create another kas

--- app/Filament/Resources/UangKasResource/Pages/CreateUangKas.php
+++ app/Filament/Resources/UangKasResource/Pages/CreateUangKas.php
@@ -4,8 +4,10 @@ namespace App\Filament\Resources\UangKasResource\Pages;
 
 use App\Filament\Resources\UangKasResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Js;
 
 class CreateUangKas extends CreateRecord
 {
@@ -25,4 +27,30 @@ class CreateUangKas extends CreateRecord
             ->title('Berhasil ditambahkan!')
             ->body('Data kas berhasil ditambahkan.');
     }
+
+    protected function getCreateFormAction(): Action
+    {
+        return Action::make('create')
+            ->label('Simpan')
+            ->submit('create')
+            ->keyBindings(['mod+s']);
+    }
+
+    protected function getCreateAnotherFormAction(): Action
+    {
+        return Action::make('createAnother')
+            ->label('Simpan dan Tambah Lagi')
+            ->action('createAnother')
+            ->keyBindings(['mod+shift+s'])
+            ->color('gray');
+    }
+
+    protected function getCancelFormAction(): Action
+    {
+        return Action::make('cancel')
+            ->label('Batal')
+            ->alpineClickHandler('document.referrer ? window.history.back() : (window.location.href = ' . Js::from($this->previousUrl ?? static::getResource()::getUrl()) . ')')
+            ->color('gray');
+    }
+
 }
