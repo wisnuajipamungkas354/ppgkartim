efedfa00fee17c07ff308b8988afdc3706af140f
Wisnu Aji Pamungkas <wisnuajipamungkas354@gmail.com>
2026-06-06 11:27:18 +0700

    Fix: calculate only verified generus

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -43,7 +43,7 @@ class LandingController extends Controller
         $type = $request->input('type'); // 'desa' | 'kelompok'
         $id   = $request->input('id');
 
-        $query = Generus::query()->withoutGlobalScopes()->whereNull('generuses.deleted_at');
+        $query = Generus::query()->withoutGlobalScopes()->where('is_verified', true)->whereNull('generuses.deleted_at');
 
         if ($type === 'desa' && $id) {
             $query->whereHas('insan', fn($q) => $q->where('desa_id', $id));
@@ -104,7 +104,7 @@ class LandingController extends Controller
      */
     private function getTotals(): array
     {
-        $counts = Generus::withoutGlobalScopes()->whereNull('generuses.deleted_at')
+        $counts = Generus::withoutGlobalScopes()->where('is_verified', true)->whereNull('generuses.deleted_at')
             ->selectRaw('kategori, COUNT(*) as total')
             ->groupBy('kategori')
             ->pluck('total', 'kategori');
